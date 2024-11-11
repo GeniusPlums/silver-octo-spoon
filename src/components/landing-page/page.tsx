2ba90c9a5d3eeddee62052f4483dfca3196a97bf
GeniusPlums <anish.adamane@gmail.com>
2024-11-11 14:36:51 +0530

    Mobile layout hamburger menu 1/3

--- src/components/landing-page/page.tsx
+++ src/components/landing-page/page.tsx
@@ -67,11 +67,11 @@ const Header: React.FC<{ isMenuOpen: boolean; setIsMenuOpen: (isOpen: boolean) =
       {/* Mobile Navigation */}
       <div className="flex lg:hidden items-center gap-4">
         <button
-          className="p-2"
+          className="p-2 text-black"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
           aria-label="Toggle menu"
         >
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <svg className="w-6 h-6" fill="none" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </button>
